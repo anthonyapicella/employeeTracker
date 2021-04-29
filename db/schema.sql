aa65ddcf341b2f9b3750e2f9f4776e9d523cb8ab
anthonyapicella <73201390+anthonyapicella@user.noreply.github.com>
2021-04-29 12:53:40 -0700

    completed schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,28 @@
-DROP DATABASE IF EXISTS library_db;
-CREATE DATABASE library_db;
+DROP DATABASE IF EXISTS employee_trackerdDB;
+
+CREATE database employee_trackerDB;
+
+USE employee_trackerDB;
+
+CREATE TABLE department (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY ,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department (id)
+);
+
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES role (id),
+    manager_id INT NULL,
+    FOREIGN KEY (manager_id) REFERENCES employee (id)
+);
--- server.js
+++ server.js
@@ -1,12 +1,16 @@
-const express = require('express');
-const sequelize = require('./config/connection');
+const mysql = require('mysql');
+const inquirer = require('inquirer');
 
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-sequelize.sync().then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: '',
+  database: 'top_songsDB',
 });
+
+connection.connect((err) => {
+  if (err) throw err;
+  runSearch();
+});
+
